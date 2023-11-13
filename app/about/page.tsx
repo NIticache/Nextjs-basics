518a9411d5c4fb9e536d8657df694a167eafb2a7
niticahe <niteeshvijayakumar500@gmail.com>
2023-11-13 15:34:31 +0530

    explore basic file based routing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  <Link href={"/about"}> Go to about</Link>
+<section>In About Page</section>
+  <Link href={"/"}> Go Back</Link>
     </main>
   )
 }
